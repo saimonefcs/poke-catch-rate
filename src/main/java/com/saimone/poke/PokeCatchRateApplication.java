4af402b969e5835191af35dde80c7e5a14d49ac8
Andrea Morandotti <andrea.morandotti@motork.io>
2022-04-04 14:39:46 +0200

    initial commit

---
+++ src/main/java/com/saimone/poke/PokeCatchRateApplication.java
@@ -0,0 +1,13 @@
+package com.saimone.poke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeCatchRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeCatchRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saimone/poke/PokeCatchRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimone.poke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeCatchRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
